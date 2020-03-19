84f0b250502e50791d947f98809539d0882531ee
TanXiaoXianSheng <1148638518@qq.com>
2020-03-19 16:49:56 +0800

    111

---
+++ eureka/server-zuul/src/test/java/cn/bcf/example/springcloud/cluster/serverzuul/ServerZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.bcf.example.springcloud.cluster.serverzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
